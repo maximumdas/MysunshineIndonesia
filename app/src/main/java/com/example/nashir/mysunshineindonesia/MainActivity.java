2c6c1de69dc19527992ed4c555e5c31f5cb2999c
maximumdas <human.balancar@gmail.com>
2018-01-13 11:47:01 +0700

    terserah

---
+++ app/src/main/java/com/example/nashir/mysunshineindonesia/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nashir.mysunshineindonesia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
